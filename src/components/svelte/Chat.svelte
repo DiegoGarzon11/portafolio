0350c41d55d04187c8a8fb27bf18e88190038dd7
Diego Garzon <diegogarzon357@gmail.com>
2023-02-22 16:14:31 -0500

    desing chat

---
+++ src/components/svelte/Chat.svelte
@@ -0,0 +1,51 @@
+<div class="flex items-center justify-center">
+	<div class="flex flex-col h-full bg-gray-100 w-4/5 rounded-lg">
+		<div class="flex-1 px-4 py-8 overflow-y-auto">
+			<div class="flex flex-col space-y-4">
+				<div class="flex items-center justify-end">
+					<div class="bg-blue-500 text-white rounded-lg px-4 py-2 max-w-xs">
+						When are you ready?
+					</div>
+				</div>
+				<div class="flex items-center justify-start">
+					<div class="bg-gray-200 text-gray-700 rounded-lg px-4 py-2 max-w-xs">
+						Diego is working on me so that I am ready to answer your questions about him
+						as soon as possible.
+					</div>
+				</div>
+				<div class="flex items-center justify-end">
+					<div class="bg-blue-500 text-white rounded-lg px-4 py-2 max-w-xs">
+						is he only working on you?
+					</div>
+				</div>
+				<div class="flex items-center justify-start">
+					<div class="bg-gray-200 text-gray-700 rounded-lg px-4 py-2 max-w-xs">
+						No, he is also improving the web design and other features
+					</div>
+				</div>
+				<div class="flex items-center justify-end">
+					<div class="bg-blue-500 text-white rounded-lg px-4 py-2 max-w-xs">Thanks!</div>
+				</div>
+				<div class="flex items-center justify-start">
+					<div class="bg-gray-200 text-gray-700 rounded-lg px-4 py-2 max-w-xs">
+						No problem, i hope help you soon!
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="px-4 py-2">
+			<div class="flex rounded-lg border-gray-400 border-2">
+				<input
+					type="text"
+					class="flex-1 rounded-l-lg p-2 focus:outline-none"
+					placeholder="Type a message..."
+				/>
+				<button
+					class="bg-blue-500 text-white rounded-r-lg px-4 py-2 hover:bg-blue-600 transition-colors"
+				>
+					Send
+				</button>
+			</div>
+		</div>
+	</div>
+</div>
