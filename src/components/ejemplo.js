44816258832b9021fb4ea01d2cc92374b8dc6ba3
Diego Garzon <diegogarzon357@gmail.com>
2023-04-13 11:22:39 -0500

    tecnologias

--- src/components/ejemplo.js
+++ src/components/ejemplo.js
@@ -19,61 +19,32 @@ window.addEventListener('load', () => {
 
 		if (mensajesUser.length <= 1) {
 			textoChat.innerText = `Nice to meet you ${nombreUser}`;
-		} else if (
-			mensaje.includes('thank') ||
-			mensaje.includes('gracias') ||
-			mensaje.includes('agradecido')
-		) {
+		} else if (mensaje.includes('thank') || mensaje.includes('gracias') || mensaje.includes('agradecido')) {
 			textoChat.innerText = "No problem , you're welcome";
-		} else if (
-			mensaje.includes('how are you') ||
-			mensaje.includes('como esta') ||
-			mensaje.includes('como vas')
-		) {
+		} else if (mensaje.includes('how are you') || mensaje.includes('como esta') || mensaje.includes('como vas')) {
 			textoChat.innerText = " I'm great, can i help you in something?";
-		} else if (
-			mensaje.includes('age') ||
-			mensaje.includes('años tiene diego') ||
-			mensaje.includes('edad') ||
-			mensaje.includes('old is diego')
-		) {
+		} else if (mensaje.includes('age') || mensaje.includes('años tiene diego') || mensaje.includes('edad') || mensaje.includes('old is diego')) {
 			textoChat.innerText = `Sure, the answer to your question: ${inputChat.value} \n \n Diego is 21`;
-		} else if (
-			mensaje.includes('hobbies') ||
-			mensaje.includes('pasatiempos') ||
-			mensaje.includes('le gusta hacer a diego')
-		) {
+		} else if (mensaje.includes('hobbies') || mensaje.includes('pasatiempos') || mensaje.includes('le gusta hacer a diego')) {
 			textoChat.innerText = ` Sure, the answer to your question: ${inputChat.value} \n \n Diego likes to do a bit of everything, like playing, soccer, video games, reading, sports, studying, languages, listening to music, software development`;
 		} else if (mensaje.includes('experiencia') || mensaje.includes('experience')) {
 			textoChat.innerText = `Sure, the answer to your question: ${inputChat.value} \n \n Diego has 1+ years of experience in development`;
 		} else if (
 			mensaje.includes('skills') ||
+			mensaje.includes('tecnologias') ||
 			mensaje.includes('habilidades') ||
 			mensaje.includes('conocimientos')
 		) {
 			textoChat.innerText = `Sure, the answer to your question: ${inputChat.value} \n \n Some of Diego' skills are javascript, typescript, angular, react, css, tailwind, astro, nodejs, express, jwt, mysql, APIs, c#, git/github, sequelize.
 			Besides that he knows english`;
-		} else if (
-			mensaje.includes('vive') ||
-			mensaje.includes('de donde es ') ||
-			mensaje.includes('nacio')
-		) {
+		} else if (mensaje.includes('vive') || mensaje.includes('de donde es ') || mensaje.includes('nacio')) {
 			textoChat.innerText = `Sure, the answer to your question: ${inputChat.value} \n \n  Diego was born and raised in Chia, Colombia, since then he has been living there`;
-		} else if (
-			mensaje.includes('haces') ||
-			mensaje.includes('what are you doing?') ||
-			mensaje.includes('que haces')
-		) {
+		} else if (mensaje.includes('haces') || mensaje.includes('what are you doing?') || mensaje.includes('que haces')) {
 			textoChat.innerText = `Sure, the answer to your question: ${inputChat.value} \n \n  i'm here for help you`;
-		} else if (
-			mensaje.includes('dedica diego') ||
-			mensaje.includes('What does Diego ?') ||
-			mensaje.includes('que hace diego')
-		) {
+		} else if (mensaje.includes('dedica diego') || mensaje.includes('What does Diego ?') || mensaje.includes('que hace diego')) {
 			textoChat.innerText = `Sure, the answer to your question: ${inputChat.value} \n \n  Diego is web developer fullstack, He is a self-taught student and studies French as well. He is improving constantly his skills and is a person who likes practice several sports`;
 		} else {
-			textoChat.innerText =
-				"Sorry, I don't have the answer right now, i hope help you as soon as possible :) ";
+			textoChat.innerText = "Sorry, I don't have the answer right now, i hope help you as soon as possible :) ";
 		}
 		btnEnviar.disabled = true;
 	}
